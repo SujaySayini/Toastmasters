3e9c91b4a68bb48486afe5079eaeead60420be37
Ryan Feng <rf445@scarletmail.rutgers.edu>
2022-02-21 18:28:18 -0500

    comment card page initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Agenda  from './components/Agenda';
+import CommentCard from './components/CommentCard';
 
 function App() {
   return (
     <div className="App">
-      <Agenda/>
+      {/*<Agenda/>*/}
+      <CommentCard/>
     </div>
   );
 }
