5613f3414397435b27f7a487bffad354c7e50e14
Jacquelynne Idowu <jai53@scarletmail.rutgers.edu>
2022-04-04 14:43:09 -0400

    users/signin/signup/resetpassword

--- client/src/actions/evaluation.js
+++ client/src/actions/evaluation.js
@@ -2,6 +2,19 @@ import * as api from '../api';
 
 //functions that return actions
 
+export const getEvaluation = (date) => async (dispatch) => {
+    try {
+        const  data2  = await api.fetchEvaluation(date);
+        console.log(data2)
+        dispatch({type: 'FETCH', payload:data2});
+        return data2;
+    } catch (error) {
+        console.log(error.message);
+        
+    }
+
+}
+
 export const createEvaluation = (evaluation) => async(dispatch) => {
     try {
         const { data } = await api.createEvaluation(evaluation)
