67c547f6964b9da53252dc38c60e565970096c05
Chiu Yin Lau <cl1309@rutgers.edu>
2022-04-04 20:18:22 -0400

    commit

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,18 +1,12 @@
 import { combineReducers } from "redux";
 import speech from './speech'
-<<<<<<< HEAD
 import clubpage from'./clubpage';
-export default combineReducers({
-    speech,
-    clubpage
-=======
 import user from './user'
 import auth from './auth'
-
 export default combineReducers({
     speech,
+    clubpage,
     user,
     auth
     //speech has create too??
->>>>>>> 9ec04c097cf2325da87f2adc7edc59286351c467
 })
