d22e55792490730e569881b74bb15f9951fbf129
Sujay Sayini <ss3214@rutgers.edu>
2022-04-07 14:43:26 -0400

    fixed cookies on timer and ah counter

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -26,21 +26,20 @@ const Timer = () => {
         }
     }, [dispatch]);*/
 
-    function getCookie(cname) {
-        let name = cname + "=";
-        let decodedCookie = decodeURIComponent(document.cookie);
-        let ca = decodedCookie.split(';');
-        for(let i = 0; i <ca.length; i++) {
-          let c = ca[i];
-          while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-          }
-          if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-          }
-        }
-        return "";
+    let user = ''
+    const cname = 'user'
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
       }
+      if (c.indexOf(name) == 0) {
+        user = JSON.parse(c.substring(name.length, c.length)).user;
+      }
+    }
 
     const updateMembers = async (club) =>{
         console.log('dispatch')
@@ -61,9 +60,9 @@ const Timer = () => {
     }
     useEffect(()=>{
         console.log('updated users')
-        let clubName = getCookie("club");
-        console.log("club names is the " + clubName);
-        updateMembers(clubName);
+        //let user = getCookie('club');
+        //console.log("club names is the " + user);
+        updateMembers(user.club);
     }, []);
 
     const clicked = async () =>{
